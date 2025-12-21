bc36ccbb3f2d0d2f6225b137e1217efabbb00725
Gokul Jayan <gokuljayan@Gokuls-MacBook-Air.local>
2025-12-21 08:31:29 +0530

    cleanup

--- src/Constants/constants.js
+++ src/Constants/constants.js
@@ -1,4 +1,4 @@
-export const baseUrl = "https://api.themoviedb.org/3";
-export const TMDB_API_KEY = "f49018237cf3fc9d46f0685d2b4e7f6d";
-export const YOUTUBE_API_KEY = "AIzaSyCTWZLa6wVefc8I68X76XM_AzW_7hPmHEc";
+export const baseUrl = "https://api.themoviedb.org/3"
 export const imageUrl = "https://image.tmdb.org/t/p/original";
+export const TMDB_API_KEY = process.env.REACT_APP_TMDB_API_KEY;
+export const YOUTUBE_API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
